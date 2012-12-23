5e02158260d5c90ed6349a73fc5ef0b86a5cf20f
Andre Furquim <andrefurkin@me.com>
2012-12-23 00:10:11 -0200

    Alterações na parte de visualização!

--- Agecel/AGLListViewController.m
+++ Agecel/AGLListViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AGLListViewController.h"
+#import "AGLState.h"
 
 @interface AGLListViewController ()
 
@@ -17,6 +18,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+  NSMutableArray *localStates = [NSMutableArray array];
+  AGLState *state = [[AGLState alloc] init];
+  state.stateName = @"São Paulo";
+  state.stateShortName=@"SP";
+  state.image = [UIImage imageNamed:@"sao-paulo.jpg"];
+  [localStates addObject:state];
+  state = [[AGLState alloc] init];
+  state.stateName = @"Paraná";
+  state.stateShortName=@"PR";
+  state.image = [UIImage imageNamed:@"parana.jpg"];
+
+  [localStates addObject:state];
+  state = [[AGLState alloc] init];
+  state.stateName = @"Santa Catarina";
+  state.stateShortName = @"SC";
+  state.image = [UIImage imageNamed:@"santa.jpg"];
+
+  [localStates addObject:state];
+  
+  self.states = localStates;
 	// Do any additional setup after loading the view.
 }
 
@@ -25,5 +46,42 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return [self.states count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  static NSString *CellIdentifier = @"Cell";
+  
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  
+  if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                  reuseIdentifier:CellIdentifier];
+  }
+  
+  cell.textLabel.text = [[self.states objectAtIndex:indexPath.row] stateName];
+  cell.detailTextLabel.text = [[self.states objectAtIndex:indexPath.row] stateShortName];
+  cell.imageView.image = [[self.states objectAtIndex:indexPath.row] image];
+
+  return cell;
+  
+}
+
+/*-
+ (void)tableView:(UITableView *)tableView
+commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+forRowAtIndexPath:(NSIndexPath *)indexPath {
+  if (editingStyle == UITableViewCellEditingStyleDelete) {
+    [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                     withRowAnimation:UITableViewRowAnimationFade];
+  } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+  }
+}
+ */
 
 @end
